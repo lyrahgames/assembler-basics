88033438f1ae68d3025ef93cc4ed7f61c48474c8
lyrahgames <lyrahgames@mailbox.org>
2021-08-21 13:48:32 +0200

    Tweak comments and use 'lea' in first example

--- 01-hello/main.s
+++ 01-hello/main.s
@@ -1,27 +1,28 @@
-# Text Section Containing the Code
-
+// Text Section Containing the Code
 .text
-# Symbol '_start' marks start address of the Program.
-# Has to be made globally accessible to be used by the linker.
+
+// Symbol '_start' marks start address of the Program.
+// Has to be made globally accessible to be used by the linker.
 .global _start
 _start:
-  # Prepare system call to write message on stdout.
+  // Prepare system call to write message on stdout.
   mov $1, %rax
-  # Write to stdout by setting first argument to 1.
+  // Write to stdout by setting first argument to 1.
   mov $1, %rdi
-  # Set start pointer to message.
-  mov $message, %rsi
-  # Provide number of characters.
+  // Set start pointer to message.
+  // mov $message, %rsi
+  lea message(%rip), %rsi
+  // Provide number of characters.
   mov $14, %rdx
   syscall
 
-  # Correctly exit the program by calling exit system call.
+  // Correctly exit the program by calling exit system call.
   mov $60, %rax
-  # Set return code.
+  // Set return code.
   mov $0, %rdi
   syscall
 
-# Data Section
+// Data Section
 .data
 message:
   .ascii "Hello, World!\n"
